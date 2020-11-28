d4484d1c1344e152cc34f449acba6f9c5f568fae
Tom French <tom@tomfren.ch>
2020-11-28 01:40:11 +0000

    feat: add Balancer logo and move NavToggle to top right

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -2,15 +2,7 @@
   <nav id="topnav" class="border-bottom position-fixed width-full">
     <div class="d-flex flex-items-center px-5" style="height: 78px;">
       <div class="flex-auto d-flex flex-items-center">
-        <!-- <a class="d-block d-xl-none " @click="toggleSidebar">
-          <Icon name="menu" size="28" class="mr-3" />
-        </a> -->
-        <NavToggle :items="links" />
-        <!-- <router-link
-          :to="{ name: 'home' }"
-          class="d-inline-block text-blue d-flex"
-          style="padding-top: 2px;"
-        >
+        <div class="d-inline-block text-black d-flex" style="padding-top: 2px;">
           <img
             src="~/@/assets/logo.svg"
             class="mr-2 v-align-middle"
@@ -22,50 +14,9 @@
             style="letter-spacing: 1px; font-size: 16px;"
             v-text="'Balancer'"
           />
-        </router-link> -->
+        </div>
       </div>
-      <!-- <div :key="web3.account">
-        <UiButton
-          v-if="$auth.isAuthenticated && !wrongNetwork"
-          @click="modalOpen.account = true"
-          :loading="loading || ui.authLoading"
-        >
-          <Avatar :address="web3.account" size="16" class="ml-n1 mr-n1" />
-          <span v-if="web3.name" v-text="web3.name" class="hide-sm ml-2 pl-1" />
-          <span
-            v-else
-            v-text="_shortenAddress(web3.account)"
-            class="hide-sm ml-2 pl-1"
-          />
-        </UiButton>
-        <UiButton
-          v-if="$auth.isAuthenticated && wrongNetwork"
-          class="button-red"
-        >
-          <Icon name="warning" class="ml-n2 mr-1 v-align-middle" />
-          {{ $t('wrongNetwork') }}
-        </UiButton>
-        <UiButton
-          v-if="!$auth.isAuthenticated && !ui.authLoading"
-          @click="modalOpen.account = true"
-          :loading="loading"
-          class="button-primary"
-        >
-          {{ $t('connectWallet') }}
-        </UiButton>
-        <router-link v-if="!wrongNetwork" :to="{ name: 'wallet' }" class="ml-2">
-          <UiButton class="v-align-bottom p-0">
-            <Icon name="wallet" size="20" class="mx-3" />
-          </UiButton>
-        </router-link>
-        <UiButton
-          v-if="myPendingTransactions.length"
-          @click="modalOpen.activity = true"
-          class="button-primary ml-2"
-        >
-          {{ myPendingTransactions.length }}
-        </UiButton>
-      </div> -->
+      <NavToggle class="v-align-middle" :items="links" />
     </div>
     <portal to="modal">
       <ModalActivity
